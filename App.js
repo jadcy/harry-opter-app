803819dafdb338a8685e24cee516839452eafdbf
Ryan Mitchell <rjmitche@usc.edu>
2020-05-30 23:50:03 -0500

    harry opter houses

--- App.js
+++ App.js
@@ -1,19 +1,53 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View, Button, Text } from 'react-native';
+// import {Text} from 'galio-framework';
 
-export default function App() {
-  return (
+
+export default class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+    House: '',
+    // buttonLoading: false,
+    buttonText: 'Sort!'
+    }
+  }
+  render(){
+    return (
     <View style={styles.container}>
-      <Text>hello</Text>
-      <Button
-  onPress={() => {
-    alert('hello judy');
-  }}
-  title="Press Me"
-/>
+      {/* <NavBar title='Sorting'></NavBar> */}
+
+      <Text>{this.state.House}</Text>
+      <Button 
+      loading={this.state.buttonLoading}
+      title={this.state.buttonText}
+      color='#6734d1'
+      onPress={() => {
+    this.setState({
+      // buttonLoading: true,
+      buttonText: 'Loading...'
+    })
+    fetch('https://www.potterapi.com/v1/sortingHat', {
+         method: 'GET'
+      })
+      .then((response) => response.json())
+      .then((responseJson) => {
+         console.log(responseJson);
+         this.setState({
+            House: responseJson,
+            buttonText: 'Sort!',
+            // buttonLoading: false
+         })
+      })
+      .catch((error) => {
+         console.error(error);
+      });
+  }} 
+></Button>
     </View>
     
   );
+}
 }
 
 const styles = StyleSheet.create({
