f9cf1f8340b4d000ac575628c2802ce914bec0b8
Jady Chan <jadychan@Jadys-Air.home>
2020-06-03 16:32:41 -0700

    useless text input

--- App.js
+++ App.js
@@ -1,49 +1,43 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Button, Text } from 'react-native';
-// import {Text} from 'galio-framework';
-
+import { StyleSheet, View, Button, Text, TextInput } from 'react-native';
 
 export default class App extends Component {
   constructor(){
     super();
     this.state = {
-    House: '',
-    // buttonLoading: false,
-    buttonText: 'Sort!'
+      House: '',
+      buttonText: 'Sort!'
     }
   }
   render(){
     return (
     <View style={styles.container}>
-      {/* <NavBar title='Sorting'></NavBar> */}
-
       <Text>{this.state.House}</Text>
-      <Button 
-      loading={this.state.buttonLoading}
-      title={this.state.buttonText}
-      color='#6734d1'
-      onPress={() => {
-    this.setState({
-      // buttonLoading: true,
-      buttonText: 'Loading...'
-    })
-    fetch('https://www.potterapi.com/v1/sortingHat', {
-         method: 'GET'
-      })
-      .then((response) => response.json())
-      .then((responseJson) => {
-         console.log(responseJson);
-         this.setState({
-            House: responseJson,
-            buttonText: 'Sort!',
-            // buttonLoading: false
-         })
-      })
-      .catch((error) => {
-         console.error(error);
-      });
-  }} 
-></Button>
+      <Button
+        loading={this.state.buttonLoading}
+        title={this.state.buttonText}
+        color='#6734d1'
+        onPress={() => {
+            this.setState({
+              buttonText: 'Loading...'
+            })
+            fetch('https://www.potterapi.com/v1/sortingHat', {
+                 method: 'GET'
+              })
+              .then((response) => response.json())
+              .then((responseJson) => {
+                 console.log(responseJson);
+                 this.setState({
+                    House: responseJson,
+                    buttonText: 'Sort!',
+                 })
+              })
+              .catch((error) => {
+                 console.error(error);
+              });
+          }}
+      ></Button>
+      <TextInput style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}/>
     </View>
 
   );
